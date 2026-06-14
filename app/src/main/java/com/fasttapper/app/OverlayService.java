5c30ad3133bf6d9e2f0b69dc4c089b9378f32b3a
gab278 <adesiyandarasimi@gmail.com>
2026-06-14 19:05:05 +0100

    Reduce TAP_INTERVAL_MS from 50 to 20

--- app/src/main/java/com/fasttapper/app/OverlayService.java
+++ app/src/main/java/com/fasttapper/app/OverlayService.java
@@ -23,7 +23,7 @@ import androidx.core.app.NotificationCompat;
 public class OverlayService extends Service {
 
     private static final String CHANNEL_ID = "fasttapper_channel";
-    private static final int TAP_INTERVAL_MS = 50;
+    private static final int TAP_INTERVAL_MS = 20;
 
     private WindowManager windowManager;
     private View overlayView;
